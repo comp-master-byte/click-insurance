16096bec9b4e58ddfc93dca3dc8b99e1c9f9850f
comp-master-byte <demoniac781@gmail.com>
2022-06-29 22:00:48 +0500

    Добавлена механика работы с селектом (плавное открытие и закрытие)

--- src/components/CustomSelect/CustomSelect.jsx
+++ src/components/CustomSelect/CustomSelect.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Pointer } from '../../icons/Pointer';
+import pointer from "../../icons/pointer.svg";
 import styles from "./CustomSelect.module.scss";
 
 export const CustomSelect = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleHandler = () => setIsOpen(prev => !prev);
+
     return (
-        <div className={styles.select}>
+        <div onClick={toggleHandler} className={styles.select}>
             <div className={styles.selectBtn}>
                 <span>Важный текст</span>
-                <Pointer />
+                <img className={isOpen ? styles.selectBtn__img : styles.selectBtn__closedImg} src={pointer} alt="" />
             </div>
-            <div className={styles.content}>
-                <ul className={styles.options}>
-                    <li>Паспорт</li>
-                    <li>Водительские права</li>
-                    <li>Военный билет</li>
-                </ul>
-            </div>
+            {isOpen &&
+                <div className={styles.content}>
+                    <ul className={styles.options}>
+                        <li>Паспорт</li>
+                        <li>Водительские права</li>
+                        <li>Военный билет</li>
+                    </ul>
+                </div>
+            }
+
         </div>
     )
 }
