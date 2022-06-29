bf92074e08612f8b82a3c620948309b61fca639a
comp-master-byte <demoniac781@gmail.com>
2022-06-29 22:30:38 +0500

    Сделал компонент управляемым и селект готов

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles/main.scss";
 import { CustomSelect } from "./components/CustomSelect/CustomSelect";
 
 function App() {
-  return (
-    <div className="wrapper">
-      <CustomSelect />
-    </div>
-  );
+
+    const [selectedOption, setSelectedOption] = useState("Паспорт");
+
+    return (
+      <div className="wrapper">
+        <CustomSelect 
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+        />
+      </div>
+    );
 }
 
 export default App;
--- src/components/CustomSelect/CustomSelect.jsx
+++ src/components/CustomSelect/CustomSelect.jsx
@@ -1,27 +1,35 @@
-import React, { useState } from 'react';
-import { Pointer } from '../../icons/Pointer';
+import React, { useState } from "react";
 import pointer from "../../icons/pointer.svg";
 import styles from "./CustomSelect.module.scss";
 
-export const CustomSelect = () => {
+export const CustomSelect = (props) => {
+
+    const { selectedOption, setSelectedOption } = props;
 
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleHandler = () => setIsOpen(prev => !prev);
 
+    const options = ['Паспорт', 'Водительские права', 'Военный билет']
+
     return (
         <div onClick={toggleHandler} className={styles.select}>
             <div className={styles.selectBtn}>
-                <span>Важный текст</span>
+                <span>{selectedOption}</span>
                 <img className={isOpen ? styles.selectBtn__img : styles.selectBtn__closedImg} src={pointer} alt="" />
             </div>
             {isOpen &&
                 <div className={styles.content}>
-                    <ul className={styles.options}>
-                        <li>Паспорт</li>
-                        <li>Водительские права</li>
-                        <li>Военный билет</li>
-                    </ul>
+                    {options.map(option =>
+                        <ul
+                            key={option}
+                            onClick={() => setSelectedOption(option)}
+                            className={styles.options}
+                        >
+                            <li>{option}</li>
+                        </ul>
+
+                    )}
                 </div>
             }
 
